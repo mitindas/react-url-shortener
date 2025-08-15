bb9c4ccb35394b51dfed25fdc18aa5eecf84d3f5
Mitin Das <mitindas304@gmail.com>
2025-08-15 15:56:18 +0530

    lets go

--- server/index.js
+++ server/index.js
@@ -1,61 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const { nanoid} = require("nanoid");
 const cors = require("cors");
-const { nanoid } = require("nanoid");
-const URLSchema = require("./models/Schema");
+const URL = require("./models/Schema");
 
-dotenv.config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: ["https://react-url-xi.vercel.app","http://localhost:3000"],
-        methods: ["POST", "GET"],
-    }
-));
-
+app.use(cors({ origin: ["https://react-url-xi.vercel.app", "http://localhost:3000"] }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
 
-const connect = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		})
-	} catch (error) {
-		console.error('Error connecting to MongoDB:', error);
-	}
-};
-
-
-//POST
+let cached = null;
+const connect = () =>
+  cached || (cached = mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }));
 
 app.post("/", async (req, res) => {
-	connect();
-	const { longUrl } = req.body;
-	const shortUrl = nanoid(7);
-	const newUrl = new URLSchema({
-		long_url: longUrl,
-		short_url: shortUrl
-	});
-	await newUrl.save();
-	res.send(process.env.APP_URL + newUrl.short_url);
+  await connect();
+  const doc = await URL.create({ long_url: req.body.longUrl, short_url: nanoid(7) });
+  res.send(process.env.APP_URL + doc.short_url);
 });
 
-//GET
-
 app.get("/:id", async (req, res) => {
-	connect();
-	const {id} = req.params;
-	const url = await URLSchema.findOne({short_url: id});
-	if (url) {
-		res.redirect(url.long_url);
-	} else {
-		res.status(404).send("URL not found");
-	}
+  await connect();
+  const doc = await URL.findOne({ short_url: req.params.id });
+  doc ? res.redirect(doc.long_url) : res.sendStatus(404);
 });
 
 module.exports = app;
